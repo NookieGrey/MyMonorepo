0bd5b5b989dea2ccf9851bfce899273f5f2920b5
NookieGrey <gaguliyanugzar@gmail.com>
2025-01-29 10:08:41 +0400

    move header to ?layout

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,12 +1,16 @@
 import {BrowserRouter, Route, Routes} from "react-router";
 import {Home} from "./pages/home/Home.tsx";
+import {HeaderComponent} from "./components/Header";
 
 export function AppRouter() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route index element={<Home/>}/>
-            </Routes>
-        </BrowserRouter>
-    )
+  return (
+    <BrowserRouter>
+      <HeaderComponent/>
+      <Routes>
+        <Route index element={<Home/>}/>
+        <Route path="/filter/:genre" element={<Home/>}/>
+        <Route path={"*"} element={<div>404 page not found</div>}/>
+      </Routes>
+    </BrowserRouter>
+  )
 }
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,11 +1,9 @@
-import {HeaderComponent} from './components/Header';
 import {FilterComponent} from './components/Filter';
 import {BookCardComponent} from './components/BookCard';
 
 export function Home() {
   return (
     <div>
-      <HeaderComponent />
       <FilterComponent />
       <BookCardComponent />
     </div>
