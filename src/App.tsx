d37e8988a77fbf1ee4376c1ecb7900131d6c1ad9
KairanI <kairangame03@gmail.com>
2025-01-30 22:23:14 +0300

    fix merge

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
-import {StrictMode} from 'react';
-import {App as AntDApp, ConfigProvider} from "antd";
-import {antdThemeConfig} from "./antdConfig.ts";
-import {AppRouter} from "./AppRouter.tsx";
-import {Provider as ReduxProvider} from "react-redux";
-import {store} from "./store.ts";
+import { StrictMode } from "react";
+import { App as AntDApp, ConfigProvider } from "antd";
+import { antdThemeConfig } from "./antdConfig.ts";
+import { AppRouter } from "./AppRouter.tsx";
+import { Provider as ReduxProvider } from "react-redux";
+import { store } from "./store.ts";
 import type Entity from "@ant-design/cssinjs/es/Cache";
-import {StyleProvider} from "@ant-design/cssinjs";
+import { StyleProvider } from "@ant-design/cssinjs";
+import "./index.css";
 
-export function App({location, cache}: { location: string, cache?: Entity }) {
+export function App({ location, cache }: { location: string; cache?: Entity }) {
   return (
     <StrictMode>
       <ReduxProvider store={store}>
         <StyleProvider cache={cache}>
           <ConfigProvider theme={antdThemeConfig}>
             <AntDApp>
-              <AppRouter location={location}/>
+              <AppRouter location={location} />
             </AntDApp>
           </ConfigProvider>
         </StyleProvider>
