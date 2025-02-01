d5202a78181fe689c913f4e08aa53915eabff680
NookieGrey <gaguliyanugzar@gmail.com>
2025-02-01 11:48:20 +0400

    i18n

---
+++ i18n.server.js
@@ -0,0 +1,30 @@
+import i18next from "i18next";
+import Backend from "i18next-fs-backend";
+import { LanguageDetector } from "i18next-http-middleware";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export function createI18nInstance() {
+  const instance = i18next.createInstance();
+
+  instance
+    .use(Backend)
+    .use(LanguageDetector)
+    .init({
+      fallbackLng: "en",
+      preload: ["en", "ru"],
+      ns: ["common", "auth"],
+      defaultNS: "common",
+      backend: {
+        loadPath: __dirname + "/locales/{{lng}}/{{ns}}.json",
+      },
+      react: {
+        useSuspense: false,
+      },
+    });
+
+  return instance;
+}
--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -1,14 +1,17 @@
-import {renderToString} from "react-dom/server";
-import {App} from "./App";
-import {createCache, extractStyle} from "@ant-design/cssinjs";
+import { renderToString } from "react-dom/server";
+import { App } from "./App";
+import { createCache, extractStyle } from "@ant-design/cssinjs";
 import type Entity from "@ant-design/cssinjs/es/Cache";
+import type { i18n } from "i18next";
 
-export function render(location: string) {
+export function render(location: string, i18n: i18n) {
   const cache: Entity = createCache();
 
-  const body = renderToString(<App cache={cache} location={location}/>);
+  const body = renderToString(
+    <App cache={cache} location={location} i18n={i18n} />,
+  );
 
   const head = extractStyle(cache);
 
-  return {body, head};
+  return { body, head };
 }
