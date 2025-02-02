af93e42f1e8f20b748416cc79edd152c1a2d28b7
NookieGrey <gaguliyanugzar@gmail.com>
2025-02-02 10:56:37 +0400

    fix vite wss

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,14 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: "frontend-wmyr.onrender.com",
+    port: 8003,
+    hmr: {
+      protocol: "wss",
+      host: "frontend-wmyr.onrender.com",
+      port: 8004,
+      clientPort: 8007,
+    },
+  },
 });
